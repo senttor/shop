5ad9ba204e1ce7ff3493a486ff13ac068f5606f4
viktor-oksentiuk-57 <voksentiuk@premierconnect.com.ua>
2019-08-04 20:52:44 +0300

    ...

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,21 +7,10 @@ use ishop\Cache;
 class MainController extends AppController {
 
     public function indexAction(){
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test', 'id = ?', [2]);
+        $brands = \R::find('brand', 'LIMIT 3');
+        $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
-        $name = 'John';
-        $age = 30;
-        $names = ['Andrey', 'Jane', 'Mike'];
-        $cache = Cache::instance();
-        //$cache->set('test', $names);
-//        $cache->delete('test');
-        $data = $cache->get('test');
-        if(!$data){
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
+        $this->set(compact('brands', 'hits'));
     }
 
 }
